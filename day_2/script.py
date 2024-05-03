860110475b0b506fef017214e3a0221e6abf7f03
SheikhUsmanJaved <j.usmansheikh@gmail.com>
2024-05-03 09:05:10 +0500

    Python learning-Day 2

---
+++ day_2/script.py
@@ -0,0 +1,22 @@
+print("Hello World") #this will run a string
+print ("Hello Akif")
+
+# this line will print a number with a + function
+print (2+4)
+print ("ide")
+
+#ctrl+/
+#variable
+
+
+
+x=3+6+9*6
+print(x)
+print(x+4)
+
+x+5 
+x+6
+
+
+
+
