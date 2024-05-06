92a954c5121a29ce619f4ddd63b3fb7e79a77c93
SheikhUsmanJaved <j.usmansheikh@gmail.com>
2024-05-06 09:05:10 +0500

    Day-3 wrote first line of program and worked on operators

---
+++ day_3/01_first_line_program.py
@@ -0,0 +1,22 @@
+print("Usman")
+print(2+3)
+print("we are learning with codanics platform")
+
+x=3+5
+print(x)
+y=x+3
+print(y)
+
+print(x+y)
+
+z=3+5-4*7/14
+print(z)
+
+print(x+y+z)
+print(x+y+z/2)
+
+print(x+y+z/2+1)
+
+print(x+y+z/2+1-1)
+print(x+y+z/2+1-1+1)
+    
---
+++ day_3/02_operators.py
@@ -0,0 +1,11 @@
+print(2+1)
+print(3-2  )
+print(6/2       ) #you will get a float number
+print(11//3) #you will get an INT
+print(4*2)
+print(13%2) # % is remainder after division
+print(2**6) #exponent 2x2x2 
+
+#PEMDAS  (Paranthesis, exponent, multiplication, division, addition, subtraction)
+
+print(3**2/2*3/3+6-4/(2*6))
