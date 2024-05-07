356d89ea3d2a5425ebac1bbd008d5fb4be53d72f
SheikhUsmanJaved <j.usmansheikh@gmail.com>
2024-05-07 09:05:10 +0500

    Day-4, Learned about Strings and comments in Python

---
+++ day_3/03_strings.py
@@ -0,0 +1,22 @@
+print("Hellow World")
+print('We are learning python')
+print("we are leaning 'python' for data science")
+print('''
+My name is Usman.
+I am Learning Pyhton for data science.
+Learning platform is codanics''')
+
+print("what's print up?")
+print("2+3+6+9")
+print(2+3+6+9)
+# print(True)
+
+
+print(2<5)
+print(2<5<10)
+
+#assignment: what is type casting and how do we use it in python
+
+#binary data and boolean operators
+True
+False
---
+++ day_3/04_comments.py
@@ -0,0 +1,21 @@
+#subject of this python script:
+#authored by: Usman Javed
+#Where to contact: github profile link "OR" email:usmanjaved07@yahoo.com (preferably gmail) google colab link up.
+
+#step-1: why to use single quotation?
+print('Usman') #when we are writing a string
+print('Whats up')
+
+#Step-2: When to use double quotation mark?
+print("Usman")
+print("What's Up?") #when writing a string and making use of single quotation mark
+
+#Step-3: When to use triple quotation mark?
+print('''
+Usman
+Javed''') #To write multiline string and for also using double and single quotation marks inside.
+
+
+# Assignment: When to use comments in python? Mention 10 study cases
+
+
